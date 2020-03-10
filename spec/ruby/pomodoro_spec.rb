2895973b9d78a30d6747c1be0308717e18cf350b
Alexander Shvaykin <skiline.alex@gmail.com>
2020-03-10 22:21:39 +0300

    remove extra spec

--- spec/ruby/pomodoro_spec.rb
+++ spec/ruby/pomodoro_spec.rb
@@ -1,9 +1,5 @@
-RSpec.describe Ruby::Pomodoro do
+describe Ruby::Pomodoro do
   it "has a version number" do
     expect(Ruby::Pomodoro::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end

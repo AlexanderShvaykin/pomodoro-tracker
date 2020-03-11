c22140142f60ac30852c98ad33228802f8ffd833
Alexander Shvaykin <skiline.alex@gmail.com>
2020-03-11 12:48:04 +0300

    fix style

--- lib/ruby/pomodoro/worker.rb
+++ lib/ruby/pomodoro/worker.rb
@@ -20,8 +20,8 @@ module Ruby
           @in_progress = true
           @do = Thread.new do
             loop do
-            sleep time_interval
-            task.track(time_interval)
+              sleep time_interval
+              task.track(time_interval)
             end
           end
           task

254063541d4ac98490b1aef14ccb9f71ab6e90b9
Alexander Shvaykin <skiline.alex@gmail.com>
2020-03-13 00:46:35 +0300

    add base implement

--- lib/ruby/pomodoro.rb
+++ lib/ruby/pomodoro.rb
@@ -6,42 +6,75 @@ require "ruby/pomodoro/progressbar"
 module Ruby
   module Pomodoro
     class Error < StandardError; end
+    PENDING_ANSWER_TIME = 60 * 5
     class << self
       def start
+        @notification = build_notification
+        @notification["stop"] = true
+        Worker.pomodoro_size = 60 * 30
         @tasks = []
 
+        puts "Hi, your tasks:"
+        puts "Count: #{@tasks.size}"
         task_list
         2.times { puts }
         commands = <<~TEXT
-            [s] - Choose task
+            [c] - Choose task to work
             [a] - Add task
             [p] - Pause work
+            [r] - Resume work
+            [s] - Stop work
             [q] - Quit
         TEXT
 
         loop do
           puts commands
+          2.times { puts }
           answer_handler(gets)
         end
+      rescue
+        finish_app
+        abort "Sorry!"
       end
 
       private
 
       def answer_handler(answer)
         2.times { puts }
+        puts answer
         case answer.to_s.downcase
         when 'q'
+          finish_app
           abort "Bye!"
         when 'a'
           add_task
+        when 's'
+          Worker.stop
+        when 'c'
+          choose_task
+        when 'p'
+          Worker.pause
+          notify("Resume?", PENDING_ANSWER_TIME)
+        when 'r'
+          @notification["stop"] = true
+          Worker.resume
         else
           answer
         end
       end
 
+      def choose_task
+        task_list
+        print "Type number task: "
+        task = @tasks[gets.to_i]
+        unless task
+          puts "Sorry, task not found!"
+          choose_task
+        end
+        Worker.do(task)
+      end
+
       def task_list
-        puts "Hi, your tasks:"
-        puts "Count: #{@tasks.size}"
         @tasks.each.with_index { |t, i| puts "#{i}. #{t.name}" }
       end
 
@@ -59,10 +92,29 @@ module Ruby
       end
 
       def notify(message, time)
+        @notification["message"] = message
+        @notification["time"] = time
+        @notification["stop"] = false
+      end
+
+      def finish_app
+        @notification.kill
+        Worker.stop
+      end
+
+      def build_notification
         Thread.new do
+          count = 0
           loop do
-            sleep time
-            `say #{message}`
+            message = Thread.current["message"]
+            sleep 1
+            count += 1 unless Thread.current["stop"]
+            if count >= Thread.current["time"].to_i
+              `say #{message}` if message
+              count = 0
+            elsif Thread.current["stop"]
+              count = 0
+            end
           end
         end
       end
